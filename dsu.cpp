7eefa2b1a7981399de66beba90ac8d86a50d90b1
m0t9 <60100612+m0t9@users.noreply.github.com>
2022-03-28 16:59:57 +0500

    Add files via upload

--- dsu.cpp
+++ dsu.cpp
@@ -1,26 +1,33 @@
-vector<ll> parents, set_sizes;
-
-ll get_parent(ll vertex) {
-    if (parents[vertex] != vertex) {
-        parents[vertex] = get_parent(parents[vertex]);
+struct Dsu {
+    vector<int> p, sz;
+    
+    Dsu(int n) {
+        p.resize(n, 1);
+        sz.resize(n);
+        iota(p.begin(), p.end(), 0);
     }
-    return parents[vertex];
-}
 
-bool in_same(ll first_vertex, ll second_vertex) {
-    return get_parent(first_vertex) == get_parent(second_vertex);
-}
-
-void join(ll first_vertex, ll second_vertex) {
-    first_vertex = get_parent(first_vertex);
-    second_vertex = get_parent(second_vertex);
-    if (first_vertex == second_vertex) {
-        return;
+    int get(int v) {
+        if (p[v] != v) {
+            p[v] = get(p[v]);
+        }
+        return p[v];
     }
-    if (set_sizes[first_vertex] > set_sizes[second_vertex]) {
-        swap(first_vertex, second_vertex);
-    }
-    parents[first_vertex] = second_vertex;
-    set_sizes[second_vertex] += set_sizes[first_vertex];
-}
 
+    void join(int v, int u) {
+        v = get(v);
+        u = get(u);
+        if (v == u) {
+            return;
+        }
+        if (sz[v] > sz[u]) {
+            swap(v, u);
+        }
+        sz[u] += sz[v];
+        p[v] = u;
+    }
+
+    bool in_same(int v, int u) {
+        return get(v) == get(u);
+    }
+}
